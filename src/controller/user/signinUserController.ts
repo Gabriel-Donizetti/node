a73841f059d0617f2b595bf9e387df1dca462554
Gabriel-Donizetti <gabsdonizetti@gmail.com>
2021-11-05 19:30:25 -0300

    feat: :art: user validation
    Validation user signin-login-updateuser with express validator

--- src/controller/user/signinUserController.ts
+++ src/controller/user/signinUserController.ts
@@ -1,8 +1,25 @@
 import { Request, Response } from 'express'
+import { validationResult } from 'express-validator';
 import { SignInUserService } from '../../services/user/signinUserService'
+
+const myValidationResult = validationResult.withDefaults({
+    formatter: error => {
+      return {
+        msg: error.msg,
+        param: error.param
+      };
+    },
+  });
 
 class SignInUserController {
     async handle(req: Request, res: Response){
+
+        const errors = myValidationResult(req);
+        console.log(errors);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+
         const { name, email, password } = req.body
 
         const service = new SignInUserService()
--- src/controller/user/updateUserController.ts
+++ src/controller/user/updateUserController.ts
@@ -1,8 +1,25 @@
 import { Request, Response } from 'express'
+import { validationResult } from 'express-validator';
 import { UpdateUserService } from '../../services/user/updateUserService'
+
+const myValidationResult = validationResult.withDefaults({
+    formatter: error => {
+      return {
+        msg: error.msg,
+        param: error.param
+      };
+    },
+  });
 
 class UpdateUserController {
     async handle(req: Request, res: Response){
+
+        const errors = myValidationResult(req);
+        console.log(errors);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+
         const { id, name, email } = req.body
 
         const service = new UpdateUserService()
